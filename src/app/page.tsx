9463192cb36b8c943b03635e7fa49571b0e42f1a
chongsu Chang <widenety@gmail.com>
2025-05-30 22:44:18 +0900

    Added test.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 //import Image from 'next/image';
 export default function Home() {
 	return (
-		<div>메인페이지</div>
+		<div>메인페이지 #3</div>
 		/*
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 			<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
