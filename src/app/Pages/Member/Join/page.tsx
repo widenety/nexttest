2c7b0305d8062ee31da58d76e7e0539fb2ddf8d2
chongsu Chang <widenety@gmail.com>
2025-05-30 21:35:18 +0900

    Setted base.

---
+++ src/app/Pages/Member/Join/page.tsx
@@ -0,0 +1,7 @@
+export default function Join() {
+	return (
+		<div>
+			Join page
+		</div>
+	);
+}
---
+++ src/app/_Layout/CustomHead.tsx
@@ -0,0 +1,12 @@
+export default function CustomHead() {
+	return (
+		<>
+			<title>Widenety's Test App</title>
+			<meta name="description" content="Widenety's Toy Project" />
+
+			<link rel="preconnect" href="https://fonts.googleapis.com" />
+			<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+			<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap" rel="stylesheet" />
+		</>
+	);
+}
