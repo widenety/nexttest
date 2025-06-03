99a8ab8c0f426669456b7f21136dfe1941b7f0f0
chongsu Chang <widenety@gmail.com>
2025-06-03 21:44:11 +0900

    Reset markup config.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,8 @@ import './_Css/common.scss';
 import './_Css/layout/layout.scss';
 import './_Css/styleGuide.scss';
 import './_Css/contents.scss';
-//import CustomHead from './_Layout/CustomHead';
-//import type { Metadata } from 'next';
-//import { Geist, Geist_Mono } from 'next/font/google';
+import type { Metadata } from 'next';
+// import { Geist, Geist_Mono } from 'next/font/google';
 
 /*
 const geistSans = Geist( {
@@ -16,22 +15,40 @@ const geistMono = Geist_Mono( {
 	variable: '--font-geist-mono',
 	subsets: [ 'latin' ],
 } );
+ // */
 export const metadata: Metadata = {
-	title: 'Widenety\'s Test App',
-	description: 'Widenety\'s Toy Project',
+	title: "Widenety's Test app",
+	description: "Widenety's Toy project",
+	/*
+	generator: "Next.js",
+	applicationName: "앱 이름",
+	referrer: "origin-when-cross-origin",
+	keywords: [ "nextjs", "app", "widenety" ],
+	authors: [ { name: "작성자", url: "https://..." } ],
+	creator: "Widenety",
+	publisher: "Widenety",
+	robots: "index, follow",
+	themeColor: "#ffffff",
+	colorScheme: "light dark",
+	category: "web app",
+	// */
 };
-// */
+
 export default function RootLayout( {
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }> ) {
 	return (
-		<html lang="ko">
+		<html lang="ko" suppressHydrationWarning>
 			<head>
-				{/* <CustomHead /> */}
-				<title>Widenety&apos;s Test App</title>
-				<meta name="description" content="Widenety&apos;s Toy Project" />
+				<meta name="format-detection" content="telephone=no" />
+				<meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+				<meta httpEquiv="Content-Style-Type" content="text/css" />
+				<meta httpEquiv="imagetoolbar" content="no" />
+				<meta httpEquiv="Pragma" content="no-cache" />
+				<meta httpEquiv="expries" content="0" />
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
 				<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap" rel="stylesheet" />
