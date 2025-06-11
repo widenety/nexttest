0a5b4a9164dfbed852fbb9fb67174fa79704c302
chongsu Chang <widenety@gmail.com>
2025-06-11 18:05:04 +0900

    Config Member join set.

--- src/app/Pages/Member/Join/page.tsx
+++ src/app/Pages/Member/Join/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState, useRef } from "react";
+import React, { useState, useRef } from "react";
 /*
 import { useEffect } from "react";
 import { db } from "@/lib/firebase";
@@ -37,55 +37,79 @@ export default function Join() {
 	// */
 
 	/* ==============================
-	* 회원유형 선택용 state
+	* 회원타입 선택
 	================================= */
-	const [ membType, setMembType ] = useState<string>( "" );
-	const typePersonalRef = useRef<HTMLInputElement>( null ); // 개인회원 라디오에 focus용
+	const [ membTypeStr, setMembTypeStr ] = useState<string>( "" );
+	const [ membTypeOk, setMembTypeOk ] = useState( false );
+	const refTypePersonal = useRef<HTMLInputElement>( null );
+	const setMbTypeChk = ( e: React.ChangeEvent<HTMLInputElement> ) => {
+		const selectedMembType = e.target.value;
+		setMembTypeStr( selectedMembType );
+		setMembTypeOk( true );
+	};
+
+	/* ==============================
+	* E-mail 유효성 검사 / 중복확인
+	================================= */
+	const [ email, setEmail ] = useState<string>( "" );
+	/** -- Firebase에서 중복 검사 */
+	const getDuplStatus = async () => {
+
+	}
+
+	const [ emailStr, setEmailStr ] = useState<string>( "" );
+	const [ emailTypeChk, setEmailTypeChk ] = useState( false );
+	const [ emailIsDupl, setEmailIsDupl ] = useState( false );
+	const [ emailOk, setEmailOk ] = useState( false );
+	const emailRef = useRef<HTMLInputElement>( null );
+	const setMbMailChk = async ( e: React.ChangeEvent<HTMLInputElement> ) => {
+		const inputedEmail = e.target.value;
+		setEmailStr( inputedEmail );
+
+		/** -- E mail 유효성 검사 먼저. */
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		const getEmailValid = emailRegex.test( inputedEmail );
+		setEmailTypeChk( getEmailValid );
+
+		/** -- E mail 유효하면 Firemase 중복확인. */
+
+		/** -- 위 조건 전부 통과시 ok Flag state OK 설정. */
+	}
+
+	/* ==============================
+	* Paswword 유효성 검사
+	================================= */
+	const [ passStr, setPassStr ] = useState<string>( "" );
+	const [ passLenLim, setPassLenLim ] = useState( 5 );
+	const [ passOk, setPassOk ] = useState( false );
+	const [ hasLim, setHasLim ] = useState( false );
+	const [ hasSpecialChar, setHasSpecialChar ] = useState( false );
+	const [ hasLetter, setHasLetter ] = useState( false );
+	const [ hasNumber, setHasNumber ] = useState( false );
+	const refPass = useRef<HTMLInputElement>( null );
+	const setMbPassChk = async ( e: React.ChangeEvent<HTMLInputElement> ) => {
+		const inputedPass = e.target.value;
+		setPassStr( inputedPass );
+
+		/** -- 최소 5글자, 특수문자 포함, 영문자포함, 숫자 포함 검사 */
+		const isMinLenOk = inputedPass.length >= passLenLim;
+		const hasSpecial = /[!@#$%^&*(),.?":{}|<>_\-\[\]\\\/+=~`]/.test( inputedPass );
+		const hasAlpha = /[a-zA-Z]/.test( inputedPass );
+		const hasNum = /[0-9]/.test( inputedPass );
+
+		setHasLim( isMinLenOk );
+		setHasSpecialChar( hasSpecial );
+		setHasLetter( hasAlpha );
+		setHasNumber( hasNum );
+
+		setPassOk( isMinLenOk && hasSpecial && hasAlpha && hasNum );
+	}
 
 	/* ==============================
 	* 회원가입
 	================================= */
 	const setJoin = async () => {
-		/** -- 회원유형 선택여부 검증 */
-		if ( !membType ) {
-			alert( "회원 유형을 선택해주세요." );
-			typePersonalRef.current?.focus(); // 개인회원 라디오에 focus
-			return;
-		}
-	}
 
-	/* ==============================
-	* 중복확인
-	================================= */
-	const [ email, setEmail ] = useState<string>( "" );
-	const [ emailCheck, setEmailCheck ] = useState( false );
-	const [ emailCheckExec, setEmailCheckExec ] = useState( false );
-	const emailRef = useRef<HTMLInputElement>( null );
-	const getIsDupl = async () => {
-		setEmailCheckExec( true );
-
-		/** -- E mail 유효성/공란란 검사 */
-		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-		if ( !email ) {
-			alert( "이메일을 입력해주세요." );
-			setEmail( "" );
-			emailRef.current?.focus();
-			setEmailCheck( false );
-			return;
-		} else if ( !email || !emailRegex.test( email ) ) {
-			alert( "올바른 이메일 형식을 입력해주세요." );
-			setEmail( "" );
-			emailRef.current?.focus();
-			setEmailCheck( false );
-			return;
-		} else {
-			alert( "올바른 이메일 형식입니다." );
-			setEmailCheck( true );
-		}
-
-		/** -- Firebase에서 중복 검사 */
-
-		/** -- 중복 검사 */
 	}
 
 	return (
@@ -105,17 +129,21 @@ export default function Join() {
 								<ul className="frmListLv2">
 									<li>
 										<div className="rdos">
-											<input type="radio" name="membType" id="Personal" value="개인회원" onChange={( e ) => setMembType( e.target.value )} ref={typePersonalRef} />
+											<input type="radio" name="membType" id="Personal" value="개인회원" onChange={setMbTypeChk} ref={refTypePersonal} />
 											<label htmlFor="Personal">개인회원</label>
 										</div>
 									</li>
 									<li>
 										<div className="rdos">
-											<input type="radio" name="membType" id="Corporate" value="기업회원" onChange={( e ) => setMembType( e.target.value )} />
+											<input type="radio" name="membType" id="Corporate" value="기업회원" onChange={setMbTypeChk} />
 											<label htmlFor="Corporate">기업회원</label>
 										</div>
 									</li>
 								</ul>
+								<div>
+									membTypeStr : {membTypeStr} <br />
+									membTypeOk : <strong className="cred">{membTypeOk ? 'true' : 'false'}</strong><br />
+								</div>
 							</dd>
 						</dl>
 					</li>
@@ -123,23 +151,18 @@ export default function Join() {
 						<dl className="frmItemLv1">
 							<dt><label>이메일</label></dt>
 							<dd className="hasEmail">
-								<input type="email" name="email" id="email" placeholder="이메일을 입력하세요." ref={emailRef} onChange={( e ) => setEmail( e.target.value )} value={email} autoComplete="off" />
-								<button type="button" className="cBtn cBtn2 mailCheck" onClick={getIsDupl}><span>중복확인</span></button>
-								{
-									emailCheckExec && (
-										emailCheck ? (
-											<p className="cInfo1 emailCheck pass">
-												<i className="ri-error-warning-line"></i>
-												<span>사용 가능한 이메일입니다.</span>
-											</p>
-										) : (
-											<p className="cInfo1 emailCheck alert">
-												<i className="ri-error-warning-line"></i>
-												<span>이메일을 입력하고 중복확인을 해주세요.</span>
-											</p>
-										)
-									)
-								}
+								<input type="email" name="email" id="email" placeholder="이메일을 입력하세요." ref={emailRef} onChange={setMbMailChk} value={emailStr} autoComplete="off" />
+								<button type="button" className="cBtn cBtn2 mailCheck" onClick={getDuplStatus}><span>중복확인</span></button>
+								<ul className="cList1 hasIcon guideMsg">
+									<li className={emailTypeChk ? 'on' : ''}><i className={`ico ri-${ emailTypeChk ? 'check' : 'close' }-line`} /><p className="txt">이메일 형태로 입력해주세요.</p></li>
+									<li><i className="ico ri-close-line" /><p className="txt">중복확인을 실행해주세요.</p></li>
+								</ul>
+								<div>
+									emailStr : {emailStr} <br />
+									emailTypeChk : {emailTypeChk ? 'true' : 'false'} <br />
+									emailIsDupl : {emailIsDupl ? 'true' : 'false'} <br />
+									emailOk : <strong className="cred">{emailOk ? 'true' : 'false'}</strong><br />
+								</div>
 							</dd>
 						</dl>
 					</li>
@@ -147,7 +170,17 @@ export default function Join() {
 						<dl className="frmItemLv1">
 							<dt><label>비밀번호</label></dt>
 							<dd className="hasPassword">
-								<input type="password" name="password" id="password" placeholder="비밀번호를 입력하세요." />
+								<input type="password" name="password" id="password" placeholder="비밀번호를 입력하세요." ref={refPass} onChange={setMbPassChk} autoComplete="off" />
+								<ul className="cList1 hasIcon guideMsg">
+									<li className={hasLim ? 'on' : ''}><i className={`ico ri-${ hasLim ? 'check' : 'close' }-line`} /><p className="txt">최소 {passLenLim}글자 이상 작성바랍니다.</p></li>
+									<li className={hasSpecialChar ? 'on' : ''}><i className={`ico ri-${ hasSpecialChar ? 'check' : 'close' }-line`} /><p className="txt">특수문자를 포함해주세요.</p></li>
+									<li className={hasLetter ? 'on' : ''}><i className={`ico ri-${ hasLetter ? 'check' : 'close' }-line`} /><p className="txt">영문자를 포함해 주세요.</p></li>
+									<li className={hasNumber ? 'on' : ''}><i className={`ico ri-${ hasNumber ? 'check' : 'close' }-line`} /><p className="txt">숫자를 포함해 주세요.</p></li>
+								</ul>
+								<div>
+									passStr: {passStr} <br />
+									passOk : <strong className="cred">{passOk ? 'true' : 'false'}</strong><br />
+								</div>
 							</dd>
 						</dl>
 					</li>
